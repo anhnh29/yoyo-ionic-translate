91b002c68b05e1a315833ec7dae7fc862d6baa1d
Nguyen Hoang Anh <hoanganh.nguyen@savvycomsoftware.com>
2024-10-03 11:20:45 +0700

    rm setup build

--- yoyo-vue-feed/src/views/HomePage.vue
+++ yoyo-vue-feed/src/views/HomePage.vue
@@ -25,22 +25,57 @@ import { onMounted, reactive, ref } from 'vue';
 import { IPostItem } from '../types';
 import PostItem from '../components/posts/PostItem.vue';
 import { Geolocation } from '@capacitor/geolocation';
+import { Storage } from '@capacitor/storage';
 
-const apiUrl = import.meta.env.VITE_API_URL;
+const postsMock = [
+  {
+    "id": 1,
+    "authorInfo": {
+      "id": 1,
+      "name": "John Doe",
+      "avatar": "https://icons.veryicon.com/png/o/miscellaneous/rookie-official-icon-gallery/225-default-avatar.png"
+    },
+    "title": "Why do we use it?",
+    "content": "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+    "comments": [
+      {
+        "id": 1,
+        "content": "comment1",
+        "userInfo": {
+          "id": 2,
+          "name": "Lisa",
+          "avatar": "https://icons.veryicon.com/png/o/miscellaneous/rookie-official-icon-gallery/225-default-avatar.png"
+        }
+      }
+    ]
+  },
+  {
+    "id": 2,
+    "authorInfo": {
+      "id": 3,
+      "name": "Alex Lunk",
+      "avatar": "https://icons.veryicon.com/png/o/miscellaneous/rookie-official-icon-gallery/225-default-avatar.png"
+    },
+    "title": "What is Lorem Ipsum?",
+    "content": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    "comments": [
+      {
+        "id": 2,
+        "content": "comment2",
+        "userInfo": {
+          "id": 1,
+          "name": "John Doe",
+          "avatar": "https://icons.veryicon.com/png/o/miscellaneous/rookie-official-icon-gallery/225-default-avatar.png"
+        }
+      }
+    ]
+  }
+]
+
 const googleApiKey = import.meta.env.VITE_GOOGLE_API_KEY;
 const country = ref('ko'); //init country code
 
 const posts = reactive<IPostItem[]>([]);
-
-const fetchData = async () => {
-  try {
-    const response = await fetch(`${apiUrl}/posts`);
-    const data = await response.json();
-    posts.push(...data);
-  } catch (error) {
-    console.error('Error fetching data:', error);
-  }
-};
 
 const getCurrentCountry = async () => {
   try {
@@ -68,9 +103,31 @@ const getCurrentCountry = async () => {
   }
 }
 
+const savePosts = async (data) => {
+  await Storage.set({
+    key: 'posts',
+    value: JSON.stringify(data),
+  });
+};
+
+const getPosts = async () => {
+  const { value } = await Storage.get({ key: 'posts' });
+  return value ? JSON.parse(value) : null;
+};
+
+
+// Save on application startup
 onMounted(() => {
-  fetchData();
+  savePosts(postsMock);
+});
+
+
+onMounted(async () => {
   getCurrentCountry();
+  const rs = await getPosts();
+  if (rs) {
+    posts.push(...rs)
+  }
 });
 
 </script>
